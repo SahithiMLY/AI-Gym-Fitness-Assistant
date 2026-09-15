d998c28a7f09cf365b2f8ff7bb627a89ae15ccd4
Sahithi MLY <sahithimalayanoor@gmail.com>
2026-09-15 13:36:17 +0530

    Allow deployed frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+   allow_origins=[
+    "http://localhost:5173",
+    "https://ai-gym-fitness-assistant-8swx.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
